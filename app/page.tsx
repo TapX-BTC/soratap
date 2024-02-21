33dbc2fc7176a80254192be06a5a9dc3a6bbcd76
btracyhurst <brandontracyhurst@gmail.com>
2024-02-20 23:10:11 -0800

    fix tg link

--- app/page.tsx
+++ app/page.tsx
@@ -149,7 +149,7 @@ export default function Home() {
         </a>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://t.me/SORA_ON_TAP"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors  hover:border-neutral-700 hover:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
