53b8894057826108834dc4ef5fae4ff681108690
btracyhurst <brandontracyhurst@gmail.com>
2024-02-20 20:08:22 -0800

    fixed byTapx button

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           contribute to this site on&nbsp;
           <code className="font-mono font-bold">github</code>
         </p>
-        <div className="z-1fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="z-1 fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://tapx.app"
