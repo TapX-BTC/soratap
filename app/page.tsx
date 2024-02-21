a717acf58c75c21ed47092e0e08c9055b1af65fa
btracyhurst <brandontracyhurst@gmail.com>
2024-02-20 22:08:46 -0800

    more mobile blur fix

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
 
         </div>
       </div>
-      <div className="relative -z-10 flex flex-col place-items-center before:z-[-10] before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:dark:bg-gradient-to-br before:dark:from-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:to-orange-500 after:blur-2xl after:content-[''] before:from-transparent before:to-orange-700 before:opacity-10 after:from-sky-900 after:via-[#ffbb01] after:opacity-40 before:lg:h-[360px] ">
+      <div className="relative -z-10 flex flex-col place-items-center before:z-[-10] before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:to-orange-500 after:blur-2xl after:content-[''] before:to-orange-700 before:opacity-10 after:from-sky-900 after:via-[#ffbb01] after:opacity-40 before:lg:h-[360px] ">
         <Image
           className="relative drop-shadow-[0_0_0.3rem_#ffffff70]"
           src="/sorafull.svg"
