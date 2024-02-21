a106710de69b57fd94474ce5381e93b41a020561
btracyhurst <brandontracyhurst@gmail.com>
2024-02-20 22:06:24 -0800

    mobile blur

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
 
         </div>
       </div>
-      <div className="relative -z-10 flex flex-col place-items-center before:z-[-10] before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:dark:bg-gradient-to-br before:dark:from-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:via-orange-400 after:to-orange-500 after:blur-2xl after:content-[''] before:from-transparent before:dark:to-orange-700 before:dark:opacity-10 after:from-sky-900 after:dark:via-[#ffbb01] after:dark:opacity-40 before:lg:h-[360px] ">
+      <div className="relative -z-10 flex flex-col place-items-center before:z-[-10] before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:dark:bg-gradient-to-br before:dark:from-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:to-orange-500 after:blur-2xl after:content-[''] before:from-transparent before:to-orange-700 before:opacity-10 after:from-sky-900 after:via-[#ffbb01] after:opacity-40 before:lg:h-[360px] ">
         <Image
           className="relative drop-shadow-[0_0_0.3rem_#ffffff70]"
           src="/sorafull.svg"
