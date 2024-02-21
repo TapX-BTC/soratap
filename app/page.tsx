10cd7dc21b0fd35ce4f96515ff263a22a1fe1ec4
btracyhurst <brandontracyhurst@gmail.com>
2024-02-20 20:15:13 -0800

    fixed height

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           contribute to this site on&nbsp;
           <code className="font-mono font-bold">github</code>
         </p>
-        <div className="z-1 fixed bg-black bottom-0 left-0 flex h-48 w-full items-end justify-center lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="z-1 fixed bg-black bottom-0 left-0 flex h-24 w-full items-end justify-center lg:static lg:h-24 lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://tapx.app"
